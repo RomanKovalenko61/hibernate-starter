a54bb1c4094c5938afb230ac75098cc78ef89a1b
RomanKovalenko61 <astek14@mail.ru>
2023-07-15 14:41:13 +0300

    add Entity

--- src/main/java/org/example/HibernateRunner.java
+++ src/main/java/org/example/HibernateRunner.java
@@ -1,17 +1,33 @@
 package org.example;
 
+import org.example.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.time.LocalDate;
+
 public class HibernateRunner {
     public static void main(String[] args) {
         Configuration configuration = new Configuration();
+//        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+//        configuration.addAnnotatedClass(User.class);
         configuration.configure("hibernate.cfg.xml");
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession()) {
-            System.out.println("OK");
+            session.beginTransaction();
+
+            var user = User.builder()
+                    .username("ivan@gmail.com")
+                    .firstname("Ivan")
+                    .lastname("Ivanov")
+                    .birthDate(LocalDate.of(2000, 1, 19))
+                    .age(20)
+                    .build();
+            session.save(user);
+
+            session.getTransaction().commit();
         }
     }
 }
